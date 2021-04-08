07174079967987616c9f58f6bc1f0b0e15c2da75
ou1z <59099381+ou1z@users.noreply.github.com>
2021-04-08 03:51:25 -0400

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,9 @@
+local Axteric = "https://raw.githubusercontent.com/ou1z/Axteric"
+local places = loadstring(game:HttpGet(Axteric .. '/main/Places.lua'))()
+_G.Axteric.Places = places
+local place = places[tostring(game.PlaceId)]
+local code = "game.Players.LocalPlayer:Kick('Unsupported Game.')"
+if place then
+    code = game:HttpGet(Axteric .. place)
+end
+loadstring(code)()
